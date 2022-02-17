43e8b4ed58d14fd1acc847a9497731f4c1205471
owenpelito64 <owen@nabepero.co.jp>
2022-02-17 14:08:19 +0800

    ACTIVITY: CRUD USING ELOQUENT

--- PHP_with_Laravel/Assets/Practice/routes/web.php
+++ PHP_with_Laravel/Assets/Practice/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -38,7 +39,7 @@ Route::get('/contact', [PostsController::class,'contact']);
 |
 */
 
-//  CREATE
+// CREATE
 //    Route::get('/insert', function(){
 //         DB::insert('insert into post (title, body) values (?, ?)', [ 'PHP with laravel', ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, atque!']);
 //       });
@@ -64,5 +65,68 @@ Route::get('/contact', [PostsController::class,'contact']);
 // Route::get('/delete', function(){
 //   DB::delete('delete from post  where id=?', [4]);
 //   return ('DELETED');
+// });
+
+/*
+|--------------------------------------------------------------------------
+| ELOQUENT
+|--------------------------------------------------------------------------
+*/
+
+
+// Route::get('/read', function(){
+//     $post = Post::all();
+//     return $posts;
+// });
+
+
+// Route::get('/find', function(){
+//     $post = Post::find(6);
+//     return $post->title;
+// });
+
+
+
+// Route::get('/findwhere',function(){
+//         $post = Post::where('id', 7)->orderBy('id', 'desc')->take (1)->get();
+//         return $post;
+// });
+
+
+// Route::get('/basicinsert', function(){
+
+//     $post = new Post;
+//     $post->title = 'Something New';
+//     $post->body = 'lorem';
+//     $post->save();
+
+//     return ('Created Successfully');
+// });
+
+
+// Route::get('/create', function(){
+//     Post::create(['title'=>'The create method', 'content'=> 'Lorem ipsum, dolor sit amet consectetur']);
+// });
+
+
+// Route::get('/update',function(){
+
+//     Post::where('id',6)->update(['title'=> 'THIS IS NOW UPDATED','body'=>"Lorem ipsum, dolor"]);
+//     return ('Updated Successfully');
+// });
+
+
+// Route::get('/delete', function(){
+//     $post = Post::find(6);
+//     $post->delete();
+
+//     return ("DELETED");
+// });
+
+// Route::get('/delete2', function(){
+
+//  Post::destroy(7);
+     
+    
 // });
 
