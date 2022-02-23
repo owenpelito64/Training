52bbdc4581d4f96660f7d6c1483ca2ab3a54cac9
owenpelito64 <owen@nabepero.co.jp>
2022-02-23 09:44:47 +0800

    Section 30 New App - Read complete

--- PHP_with_Laravel/Assets/new_app/resources/views/home.blade.php
+++ PHP_with_Laravel/Assets/new_app/resources/views/home.blade.php
@@ -6,47 +6,25 @@
         <small>Secondary Text</small>
       </h1>
 
+      @foreach ($posts as $post)
+          
+   
+
       <!-- Blog Post -->
       <div class="card mb-4">
-        <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+        <img class="card-img-top" src="{{$post->post_image}}" alt="Card image cap">
         <div class="card-body">
-          <h2 class="card-title">Post Title</h2>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-          <a href="#" class="btn btn-primary">Read More &rarr;</a>
+          <h2 class="card-title">{{$post->title}}</h2>
+          <p class="card-text">{{$post->body}}</p>
+          <a href="{{$post->user_id}}" class="btn btn-primary">Read More &rarr;</a>
         </div>
         <div class="card-footer text-muted">
-          Posted on January 1, 2017 by
+          {{$post->created_at->diffForHumans()}}
           <a href="#">Start Bootstrap</a>
         </div>
       </div>
 
-      <!-- Blog Post -->
-      <div class="card mb-4">
-        <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
-        <div class="card-body">
-          <h2 class="card-title">Post Title</h2>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-          <a href="#" class="btn btn-primary">Read More &rarr;</a>
-        </div>
-        <div class="card-footer text-muted">
-          Posted on January 1, 2017 by
-          <a href="#">Start Bootstrap</a>
-        </div>
-      </div>
-
-      <!-- Blog Post -->
-      <div class="card mb-4">
-        <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
-        <div class="card-body">
-          <h2 class="card-title">Post Title</h2>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-          <a href="#" class="btn btn-primary">Read More &rarr;</a>
-        </div>
-        <div class="card-footer text-muted">
-          Posted on January 1, 2017 by
-          <a href="#">Start Bootstrap</a>
-        </div>
-      </div>
+      @endforeach
 
     @endsection
     
--- PHP_with_Laravel/Section Notes/Section30_Laravel_Fundamentals_New_App.md
+++ PHP_with_Laravel/Section Notes/Section30_Laravel_Fundamentals_New_App.md
@@ -1,10 +1,84 @@
 # Section 30 Creating New App
 
-
-
 **WORK FLOW**
 
 - create proj setup template setup database setup Authentication
 
+```
 
 $ php artisan make:factory PostFactory --model=Post
+
+```
+
+**CRUD WORKFLOW**
+
+- [ ] CREATE
+- [x] READ
+
+- pass the post model to home controller to pass in Post data
+
+```php
+
+
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+
+       $posts = Post::all();
+        return view('home', ['posts' => $posts]);
+    }
+}
+
+
+
+```
+
+- Go to home view and display the data according to the card
+
+```php
+
+
+ @foreach ($posts as $post)
+
+      <!-- Blog Post -->
+      <div class="card mb-4">
+        <img class="card-img-top" src="{{$post->post_image}}" alt="Card image cap">
+        <div class="card-body">
+          <h2 class="card-title">{{$post->title}}</h2>
+          <p class="card-text">{{$post->body}}</p>
+          <a href="{{$post->user_id}}" class="btn btn-primary">Read More &rarr;</a>
+        </div>
+        <div class="card-footer text-muted">
+          {{$post->created_at->diffForHumans()}}
+          <a href="#">Start Bootstrap</a>
+        </div>
+      </div>
+
+      @endforeach
+
+```
+
+- [ ] UPDATE
+- [ ] DELETE
